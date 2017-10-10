b9833836de3e113be05bd6be4ef21fddce198920
keisei_1092 <keiseis1092@gmail.com>
2017-10-10 18:43:41 +0900

    Initial Commit

---
+++ CameraCropPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CameraCropPractice
+//
+//  Created by Keisei Saito on 2017/10/10.
+//  Copyright © 2017 Keisei Saito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
